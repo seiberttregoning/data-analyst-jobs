2ce5fdfcd778914f77a4bdb80c0b121ce95b9a83
seiberttregoning <59902956+seiberttregoning@users.noreply.github.com>
2020-01-18 13:56:22 -0600

    Question 12 done

--- scripts.sql
+++ scripts.sql
@@ -136,3 +136,27 @@ SELECT COUNT(DISTINCT title)
 FROM data_analyst_jobs
 WHERE LOWER(title) LIKE '%analyst%';
 */
+
+/* 12.	How many different job titles do not 
+contain either the word ‘Analyst’ or the word ‘Analytics’?
+What word do these positions have in common?
+4. Tableau
+
+SELECT DISTINCT title
+FROM data_analyst_jobs
+WHERE 
+title NOT LIKE '%Analyst%' AND 
+title NOT LIKE '%analyst%' AND
+title NOT LIKE '%ANALYST%' AND
+title NOT LIKE '%Analytics%' AND
+title NOT LIKE '%analytics%' AND
+title NOT LIKE '%ANALYTICS%'
+;
+
+SELECT DISTINCT title
+FROM data_analyst_jobs
+WHERE 
+LOWER(title) NOT LIKE '%analyst%' AND 
+LOWER(title) NOT LIKE '%analytics%'
+;
+*/
