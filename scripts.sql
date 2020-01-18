7f5973ea84fa74b4cb79091f44eaf7b860675fba
seiberttregoning <59902956+seiberttregoning@users.noreply.github.com>
2020-01-18 13:26:47 -0600

    Question 2 message

--- scripts.sql
+++ scripts.sql
@@ -1,7 +1,19 @@
 /* 1.	How many rows are in the data_analyst_jobs table?
 
 1793
-*/
+
 
 SELECT COUNT(*)
 FROM data_analyst_jobs;
+*/
+
+/* 2.	Write a query to look at just the first 10 rows. 
+What company is associated with the job posting on the 10th row? 
+
+ExxonMobil
+
+
+SELECT company
+FROM data_analyst_jobs
+LIMIT 10;
+*/
