8b1aeac9824be20a21fbbccfab13433cae66d87e
seiberttregoning <59902956+seiberttregoning@users.noreply.github.com>
2020-01-18 13:50:13 -0600

    Question 8 done

--- scripts.sql
+++ scripts.sql
@@ -75,4 +75,10 @@ SELECT COUNT(DISTINCT title)
 FROM data_analyst_jobs;
 */
 
+/* 8.	How many unique job titles are there for California companies?
+230
 
+SELECT COUNT(DISTINCT title)
+FROM data_analyst_jobs
+WHERE location = 'CA';
+*/
